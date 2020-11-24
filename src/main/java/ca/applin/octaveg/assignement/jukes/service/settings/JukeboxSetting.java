86f94365346b4c0f878584f64fc4136544e5a158
Applin <lapplin.olivier@gmail.com>
2020-11-24 03:49:03 -0500

    remove coment + readme

--- src/main/java/ca/applin/octaveg/assignement/jukes/service/settings/JukeboxSetting.java
+++ src/main/java/ca/applin/octaveg/assignement/jukes/service/settings/JukeboxSetting.java
@@ -1,7 +1,5 @@
 package ca.applin.octaveg.assignement.jukes.service.settings;
 
-import ca.applin.octaveg.assignement.jukes.service.jukebox.Jukebox;
-import ca.applin.octaveg.assignement.jukes.service.jukebox.JukeboxComponent;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Set;
 import lombok.AllArgsConstructor;
@@ -16,12 +14,6 @@ public class JukeboxSetting {
     @JsonProperty
     private String id;
 
-    /**
-     * The required Components for this setting to be supported. May be empty.
-     *
-     * @see Jukebox
-     * @see JukeboxComponent
-     */
     @JsonProperty
     private Set<String> requires;
 }
